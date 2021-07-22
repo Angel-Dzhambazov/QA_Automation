c09ffe2d1b89e6da23c56247aba8430ce52178b0
Angel Dzhambazov <angel.dzhambazov@gmail.com>
2021-07-22 13:12:36 +0300

    update via front end

--- src/com/estafet/learning/Main.java
+++ src/com/estafet/learning/Main.java
@@ -2,6 +2,6 @@ package com.estafet.learning;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Estafet!");
+        System.out.println("This text is edited via github front end!");
     }
 }
