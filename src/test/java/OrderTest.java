894a4d651008c311b26b04020a4bac2504abdbc5
Angel Dzhambazov <angel.dzhambazov@estafet.com>
2021-07-28 10:30:52 +0300

    adding Order and TradeInvoice tests

---
+++ src/test/java/OrderTest.java
@@ -0,0 +1,24 @@
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.logging.Logger;
+
+import static org.junit.Assert.*;
+
+
+public class OrderTest {
+    private static Logger log = Logger.getLogger(OrderTest.class.getName());
+
+
+    @Before
+    public void setUp() throws Exception {
+
+
+    }
+
+    @Test
+    public void testOrder() {
+    }
+}
---
+++ src/test/java/TradeInvoiceTest.java
@@ -0,0 +1,67 @@
+import com.estafet.learning.TradeInvoice;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.logging.Logger;
+
+import static org.junit.Assert.*;
+
+
+public class TradeInvoiceTest {
+
+    private static Logger log = Logger.getLogger(TradeInvoiceTest.class.getName());
+    private static TradeInvoice testInvoice;
+
+    @Before
+    public void setUp() throws Exception {
+        //initialize default TestInvoice
+        testInvoice = generateDefaultInvoice();
+    }
+
+    @Test
+    public void makeDiscount() {
+
+        log.info("executing test #1");
+        log.info("This is a warning message!");
+
+        testInvoice.setItemPrice(new long[3]);
+        long initialTotalAmountInvoice = testInvoice.getTotalAmountInvoice();
+        log.info("initialTotalAmountInvoice = " + initialTotalAmountInvoice);
+        testInvoice.discountCalculation();
+
+        long discountedTotalAmountInvoice = testInvoice.getTotalAmountInvoice();
+        log.info("discountedTotalAmountInvoice = " + discountedTotalAmountInvoice);
+
+        assertEquals("this is not equal", discountedTotalAmountInvoice, initialTotalAmountInvoice);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+        testInvoice = null;
+    }
+
+    private static TradeInvoice generateDefaultInvoice() {
+
+        String clientDetailsInvoice = "TestClient";
+        String[] listWithArticlesInvoice = new String[10];
+        long totalAmountInvoice = 230;
+        int invoiceNumber = 1;
+        String bankAccountIndexInvoice = "BAII";
+        int dateOfReleaseInvoice = 20210728;
+        long businessDiscountInvoice = 0;
+        int vaTInvoice = 17;
+
+
+        return new TradeInvoice(clientDetailsInvoice, listWithArticlesInvoice, totalAmountInvoice, invoiceNumber,
+                        bankAccountIndexInvoice,
+                        dateOfReleaseInvoice, businessDiscountInvoice, vaTInvoice);
+
+
+
+    }
+
+}
+
+
