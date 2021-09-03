c5fd2799d6761a721ac26d426a4cada93fe9b20a
Angel Dzhambazov <angel.dzhambazov@estafet.com>
2021-09-03 10:59:49 +0300

    examples table pretty

--- src/test/java/com/estafet/learning/stepDefinitions/selenium/bg/lillyDrogerie/BuyProducts_Lilly_Steps.java
+++ src/test/java/com/estafet/learning/stepDefinitions/selenium/bg/lillyDrogerie/BuyProducts_Lilly_Steps.java
@@ -99,6 +99,8 @@ public class BuyProducts_Lilly_Steps {
 
         shippingPage.enterAddress(lillyReader.getAddress());
 
+        shippingPage.chooseOffice();
+
     }
 
     @And("^delivery cost should be (.*)$")
