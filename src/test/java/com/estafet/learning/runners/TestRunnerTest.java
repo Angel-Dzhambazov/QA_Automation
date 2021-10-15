a963a17600a19cbd94f172b0ea50962c7cc104a3
Angel Dzhambazov <angel.dzhambazov@estafet.com>
2021-10-15 15:53:13 +0300

    update URI

--- src/test/java/com/estafet/learning/runners/TestRunnerTest.java
+++ src/test/java/com/estafet/learning/runners/TestRunnerTest.java
@@ -7,7 +7,7 @@ import org.junit.runner.RunWith;
 
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/resources/features",
+@CucumberOptions(features = "classpath:/features/selenium/bg.bard",
         plugin = {"pretty", "html:target/cucumber-report.html", "json:target/cucumber-report2.json"},
         tags = "@bardbg",
         glue = {"com.estafet.learning.stepDefinitions"})
