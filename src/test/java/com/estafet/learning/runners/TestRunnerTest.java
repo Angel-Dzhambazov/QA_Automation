62e692dcaba3b3d2558866c74a267bfc7508dbd8
Angel Dzhambazov <angel.dzhambazov@estafet.com>
2021-10-15 16:00:45 +0300

    rename cucumber report

--- src/test/java/com/estafet/learning/runners/TestRunnerTest.java
+++ src/test/java/com/estafet/learning/runners/TestRunnerTest.java
@@ -8,7 +8,7 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "classpath:/features/selenium/bg.bard",
-        plugin = {"pretty", "html:target/cucumber-report.html", "json:target/cucumber-report2.json"},
+        plugin = {"pretty", "html:target/cucumber-report.html", "json:target/cucumber-report.json"},
         tags = "@bardbg",
         glue = {"com.estafet.learning.stepDefinitions"})
 public class TestRunnerTest {
