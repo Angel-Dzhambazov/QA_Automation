91d993e3f773a05d5fa45afbf72e3e6758518c6e
Angel Dzhambazov <angel.dzhambazov@estafet.com>
2021-10-15 15:15:40 +0300

    remove WSDL

--- src/test/java/com/estafet/learning/api/soap/wsdl/Wsdl.java
+++ src/test/java/com/estafet/learning/api/soap/wsdl/Wsdl.java
@@ -17,19 +17,19 @@ public class Wsdl {
         String endPointUrl = "http://webservices.oorsprong.org/websamples.countryinfo/CountryInfoService.wso?WSDL";
 
 
-        WsdlProject project = new WsdlProject();
-
-        WsdlInterface[] wsdls = WsdlImporter.importWsdl(project, endPointUrl);
-        WsdlInterface wsdl = wsdls[0];
-
-
-        for (Operation operation : wsdl.getOperationList()) {
-            WsdlOperation op = (WsdlOperation) operation;
-            System.out.println("OP:" + op.getName());
-            System.out.println(op.createRequest(true));
-            System.out.println("Response:");
-            System.out.println(op.createResponse(true));
-        }
+//        WsdlProject project = new WsdlProject();
+//
+//        WsdlInterface[] wsdls = WsdlImporter.importWsdl(project, endPointUrl);
+//        WsdlInterface wsdl = wsdls[0];
+//
+//
+//        for (Operation operation : wsdl.getOperationList()) {
+//            WsdlOperation op = (WsdlOperation) operation;
+//            System.out.println("OP:" + op.getName());
+//            System.out.println(op.createRequest(true));
+//            System.out.println("Response:");
+//            System.out.println(op.createResponse(true));
+//        }
 
     /*
         WsdlOperation op = (WsdlOperation) operation;
