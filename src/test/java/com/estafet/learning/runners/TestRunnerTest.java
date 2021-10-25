768cdf1b471b6fc3053703ef16c7ce81fa735a5a
Angel Dzhambazov <angel.dzhambazov@estafet.com>
2021-10-25 10:35:44 +0300

    update feature location

--- src/test/java/com/estafet/learning/runners/TestRunnerTest.java
+++ src/test/java/com/estafet/learning/runners/TestRunnerTest.java
@@ -5,7 +5,7 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "classpath:/features/selenium/bg.bard",
+@CucumberOptions(features = "classpath:/features",
         plugin = {"pretty", "html:target/cucumber-report.html", "json:target/cucumber-report.json"},
         tags = "@bardbg or @SOAPcalc",
         glue = {"com.estafet.learning.stepDefinitions"})
